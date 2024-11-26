76f0421999657bd7036e0369f6a0585083ab95b1
wjwj910 <wjwj910@gmail.com>
2024-11-26 16:38:31 +0900

    level 5 commit

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,25 +1,38 @@
 package com;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public void run(){
+    static int lastId = 0;
+    ArrayList<WiseSaying> wiseSayingList = new ArrayList<>();
+
+    public void run() {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
-        int lastId = 0;
-        while(true){
+
+        while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if(cmd.equals("종료")){
+            if (cmd.equals("종료")) {
                 break;
-            } else if(cmd.equals("등록")){
+            } else if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
                 String content = scanner.nextLine();
                 System.out.print("작가 : ");
                 String author = scanner.nextLine();
+
                 int id = ++lastId;
+                WiseSaying wiseSaying = new WiseSaying(id, author, content);
+                wiseSayingList.add(wiseSaying);
                 System.out.println(id + "번 명언이 등록되었습니다.");
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (WiseSaying wiseSaying : wiseSayingList) {
+                    System.out.println(wiseSaying);
+                }
             }
         }
     }
---
+++ src/main/java/com/WiseSaying.java
@@ -0,0 +1,21 @@
+package com;
+
+public class WiseSaying {
+    private int id;
+    private String author;
+    private String content;
+
+    public WiseSaying(int id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+    public int getId() { return id; }
+    public String getAuthor() { return author; }
+    public String getContent() { return content; }
+
+    @Override
+    public String toString() {
+        return id + " / " + author + " / " + content;
+    }
+}
