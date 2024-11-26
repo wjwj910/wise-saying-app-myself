a22c553191fff7b35c731b2d458a66c18f857925
wjwj910 <wjwj910@gmail.com>
2024-11-26 16:22:15 +0900

    level 2 commit

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -10,8 +10,14 @@ public class App {
         while(true){
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
+
             if(cmd.equals("종료")){
                 break;
+            } else if(cmd.equals("등록")){
+                System.out.print("명언 : ");
+                String content = scanner.nextLine();
+                System.out.print("작가 : ");
+                String author = scanner.nextLine();
             }
         }
     }
