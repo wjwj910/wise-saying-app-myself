40e73c036a5b1061983ee64c1a7ecad61a9f8100
wjwj910 <wjwj910@gmail.com>
2024-11-26 16:59:14 +0900

    level 7 commit

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -24,19 +24,35 @@ public class App {
                 String author = scanner.nextLine();
 
                 int id = ++lastId;
+
                 WiseSaying wiseSaying = new WiseSaying(id, author, content);
                 wiseSayingList.add(wiseSaying);
                 System.out.println(id + "번 명언이 등록되었습니다.");
             } else if (cmd.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("----------------------");
-                for (WiseSaying wiseSaying : wiseSayingList.reversed()) {
-                    System.out.println(wiseSaying);
+
+                for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
+                    System.out.println(wiseSayingList.get(i));
                 }
             } else if (cmd.startsWith("삭제")){
                 int delID = Integer.parseInt(cmd.substring(6));
-                wiseSayingList.remove(delID - 1);
-                System.out.println(delID+ "번 명언이 삭제되었습니다.");
+                WiseSaying wiseSayingToDel = null;
+
+                //존재하지 않는 명언 삭제 예외 처리
+                for (WiseSaying wiseSaying : wiseSayingList) {
+                    if (wiseSaying.getId() == delID) {
+                        wiseSayingToDel = wiseSaying;
+                        break;
+                    }
+                }
+                if(wiseSayingToDel != null) {
+                    wiseSayingList.remove(wiseSayingToDel);
+                    System.out.println(delID + "번 명언이 삭제되었습니다.");
+                } else {
+                    System.out.println(delID + "번 명언은 존재하지 않습니다.");
+                }
+
             }
         }
     }
