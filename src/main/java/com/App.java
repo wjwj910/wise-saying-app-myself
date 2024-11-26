d6b71468021e69e47dfb7a50536805b4478b60d2
wjwj910 <wjwj910@gmail.com>
2024-11-26 17:23:30 +0900

    level 8 commit

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -35,24 +35,50 @@ public class App {
                 for (int i = wiseSayingList.size() - 1; i >= 0; i--) {
                     System.out.println(wiseSayingList.get(i));
                 }
-            } else if (cmd.startsWith("삭제")){
+            } else if (cmd.startsWith("삭제?id=")) {
                 int delID = Integer.parseInt(cmd.substring(6));
                 WiseSaying wiseSayingToDel = null;
 
-                //존재하지 않는 명언 삭제 예외 처리
+                // 존재하지 않는 명언 삭제 예외 처리
                 for (WiseSaying wiseSaying : wiseSayingList) {
                     if (wiseSaying.getId() == delID) {
                         wiseSayingToDel = wiseSaying;
                         break;
                     }
                 }
-                if(wiseSayingToDel != null) {
+                if (wiseSayingToDel != null) {
                     wiseSayingList.remove(wiseSayingToDel);
                     System.out.println(delID + "번 명언이 삭제되었습니다.");
                 } else {
                     System.out.println(delID + "번 명언은 존재하지 않습니다.");
                 }
+            } else if (cmd.startsWith("수정?id=")) {
+                int modifyID = Integer.parseInt(cmd.substring(6));
+                WiseSaying wiseSayingToModify = null;
 
+                // 수정할 명언 찾기
+                for (WiseSaying wiseSaying : wiseSayingList) {
+                    if (wiseSaying.getId() == modifyID) {
+                        wiseSayingToModify = wiseSaying;
+                        break;
+                    }
+                }
+                if (wiseSayingToModify != null) {
+                    System.out.println("명언(기존) : " + wiseSayingToModify.getContent());
+                    System.out.print("명언 : ");
+                    String modifyContent = scanner.nextLine(); // 새로운 명언 입력 받기
+                    System.out.println("작가(기존) : " + wiseSayingToModify.getAuthor());
+                    System.out.print("작가 : ");
+                    String modifyAuthor = scanner.nextLine(); // 새로운 작가 입력 받기
+
+                    // 기존 명언과 작가를 새로운 값으로 교체
+                    wiseSayingToModify.setContent(modifyContent);
+                    wiseSayingToModify.setAuthor(modifyAuthor);
+
+                    System.out.println(modifyID + "번 명언이 수정되었습니다.");
+                } else {
+                    System.out.println(modifyID + "번 명언은 존재하지 않습니다.");
+                }
             }
         }
     }
--- src/main/java/com/WiseSaying.java
+++ src/main/java/com/WiseSaying.java
@@ -11,8 +11,14 @@ public class WiseSaying {
         this.content = content;
     }
     public int getId() { return id; }
+
     public String getAuthor() { return author; }
+
+    public void setAuthor(String modifyAuthor) { this.author = modifyAuthor; }
+
     public String getContent() { return content; }
+
+    public void setContent(String modifyContent) { this.content = modifyContent; }
 
     @Override
     public String toString() {
